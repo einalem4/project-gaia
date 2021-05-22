3f7659bb99aae664026a5a115408e2b263aa863b
Nikki G <nikki.goodridge@gmail.com>
2021-05-22 18:32:07 -0400

    login and sign up with no auth yet

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import CreateEvent from './pages/CreateEvent';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 const client = new ApolloClient({
   request: operation => {
@@ -22,6 +24,8 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <Switch>
+          <Route exact path ='/login' component={Login} />
+          <Route exact path ='/signup' component={SignUp} />
           <Route exact path="/create-event" component={CreateEvent} />
         </Switch>
       </Router>
