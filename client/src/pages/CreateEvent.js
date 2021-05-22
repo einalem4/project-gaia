58d943d45a381ba0e7ba24b07d6406e4c18c980b
einalem4 <melanie@codehangar.io>
2021-05-22 11:49:59 -0600

    add zipcode field

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -121,6 +121,10 @@ const CreateEvent = () => (
         <option value="WY">Wyoming</option>
       </select>
     </div>
+    <div class="field">
+    <label>Zip code</label>
+          <input type="text" name="zipcode[zip-code]" maxlength="5" placeholder="Zip code"/>
+        </div>
     <Form.TextArea label='About' placeholder='Tell us about your event' />
     <Button type='submit'>Create Event</Button>
   </Form>
