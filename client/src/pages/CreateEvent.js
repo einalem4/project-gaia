cda8db96596b70cd31725a7db5a0ecb5b6fddd53
einalem4 <melanie@codehangar.io>
2021-05-21 22:15:32 -0600

    add date picker

--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react'
+import { useState } from "react";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
+
+function AntDatepicker() {
+  const [date, setDate] = useState(new Date());
+
+  function onChange(date, dateString) {
+    setDate(date);
+  }
+
+  return <DatePicker onChange={onChange} />;
+}
 
 const CreateEvent = () => (
   <Form>
@@ -7,6 +20,7 @@ const CreateEvent = () => (
       <label>Event Name</label>
       <input placeholder='Event Name' />
     </Form.Field>
+    <AntDatepicker/>
     <Form.TextArea label='About' placeholder='Tell us about your event' />
     <Button type='submit'>Create Event</Button>
   </Form>
