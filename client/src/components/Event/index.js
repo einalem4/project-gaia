165e2eb8ba1f59af2aa0ba271d6b7a75269badfa
Nikki G <nikki.goodridge@gmail.com>
2021-05-26 19:33:20 -0400

    added design template for events;

--- client/src/components/Event/index.js
+++ client/src/components/Event/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
 
 
 function Event() {
     return (
+        <>
         <Card style={{ width: '18rem' }}>
         <Card.Title>Event Name</Card.Title>
-        <Card.Text>Date:</Card.Text>
-        <Card.Text>Time:</Card.Text>
-        <Card.Text>Address:</Card.Text>
+        <Card.Text>📆 Date:</Card.Text>
+        <Card.Text>🕓 Time:</Card.Text>
+        <Card.Text>📍 Address:</Card.Text>
          <Card.Img variant="top" src="holder.js/100px180" />
-         <Card.Title>Event Organizer: </Card.Title>
+         <Card.Title>🗣 Event Organizer: </Card.Title>
          <Card.Body>
             <Card.Text>
                 Event Description
-             Some quick example text to build on the card title and make up the bulk of
-              the card's content.
              </Card.Text>
-               <Button variant="primary">RSVP</Button>
+               <Button variant="success">RSVP</Button>
             </Card.Body>
         </Card>
+        
+        <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Label></Form.Label>
+        <Form.Control as="textarea" rows={3} placeholder='Leave Comment Here'/>
+        <br/>
+        <Button variant='primary'>Post Comment</Button>
+        </Form.Group>
+        </Form>
+        </>
     )
 }
 
