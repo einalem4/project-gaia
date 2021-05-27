e03d3899bee4c33036254674c1eada5cd63fed57
Ian Jackson <ijacksondesign@gmail.com>
2021-05-26 21:27:34 -0400

    fixed logo redirecting and added back in conditional rendering of navigation menu

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { Button, Container, FormControl, InputGroup, Jumbotron } from 'react-bootstrap';
+import Search from '../components/Search';
+import About from '../components/About';
 
 const Home = () => {
     return(
-        <Jumbotron>
-            <Container className='search-container d-flex flex-column justify-content-center' fluid>
-                <h2>cleaning the world,<br /> one community at a time</h2>
-                <p>Find a community cleanup near you</p>
-                <InputGroup className='d-flex justify-content-center'>
-                    <FormControl
-                        placeholder="Enter Zipcode to find events near you"
-                        aria-label="Search by Zipcode"
-                    />
-                    <InputGroup.Append>
-                        <Button variant='secondary'>Search</Button>
-                    </InputGroup.Append>
-                </InputGroup>
-            </Container>
-        </Jumbotron>
+        <>
+            <Search />
+            <About />
+        </>
     );
 };
 
