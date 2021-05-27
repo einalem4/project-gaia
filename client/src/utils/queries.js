709a34b7c52abf07920025d8a5d47aa2db7cfece
einalem4 <melanie@codehangar.io>
2021-05-26 18:26:35 -0600

    update event mutation and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const QUERY_EVENTS = gql`
-  query events($username: String) {
-    events(username: $username) {
+  query events($eventData: String!, $name: String!, $date: String!, $time: String!, $address1: String!, $address2: String!, $city: String!, $state: String!, $zip: String!, $description: String!) {
+    events(eventData: $eventData) {
       _id
       eventData
       createdAt
