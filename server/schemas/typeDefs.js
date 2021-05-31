e254a9bfec96ef5c6732be49b6fc6c6905aec7f5
Nikki G <nikki.goodridge@gmail.com>
2021-05-30 22:24:00 -0400

    fixed all stupid f-ing issues

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,10 @@
 const { gql } = require('apollo-server-express');
-
 // import the gql tagged template function
 const typeDefs = gql`
     type Auth {
         token: ID!
         user: User
     }
-
     type User {
         _id: ID
         username: String
@@ -17,7 +15,6 @@ const typeDefs = gql`
         friendCount: Int
         image: String
     }
-
     input EventInput {
         name: String!
         date: String!
@@ -28,7 +25,6 @@ const typeDefs = gql`
         zip: String!
         description: String!
       }
-
       type Event {
         _id: ID
         name: String!
@@ -44,21 +40,12 @@ const typeDefs = gql`
         image: String
         createdAt: String!
     }
-
     type Comment {
         _id: ID
         commentText: String
         username: String
         createdAt: String
     }
-
-    type Comment {
-        _id: ID
-        commentText: String
-        username: String
-        createdAt: String
-    }
-
     type Query {
         me: User
         users: [User]
@@ -68,27 +55,13 @@ const typeDefs = gql`
         singleEvent(_id: ID!): Event
         searchEvents(city: String!): [Event]
     }
-
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-<<<<<<< HEAD
-<<<<<<< HEAD
-        addEvent(eventData: String!): Event
-        addComment(commentText: String!): Comment
-
-    }
-=======
-        addEvent(eventData: EventInput): Event
-      }
->>>>>>> 9345edf94df391689f12d167b3779756254dfc63
-=======
         addEvent(input: EventInput): Event
         addComment(eventId: ID!, commentText: String!): Event
         addFriend(friendId: ID!): User
     }
->>>>>>> 66540aabe1e978132c596e3c4363205c42ab34db
 `;
-
 // export the typeDefs
 module.exports = typeDefs;
