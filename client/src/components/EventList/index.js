2f7c271d651ad3c69cc9cf20749b97185a3fa923
einalem4 <melanie@codehangar.io>
2021-05-31 00:00:26 -0600

    style created events and friends section in user profile

--- client/src/components/EventList/index.js
+++ client/src/components/EventList/index.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card,  Container, Row, Col } from 'react-bootstrap';
 
-const EventList = ({ events, title }) => {
+
+const EventList = ({ events }) => {
   if (!events.length) {
-    return <h3>You have not created any events yet</h3>;
+    return <Card.Body><Card.Text><h3>You have not created any events yet</h3></Card.Text></Card.Body>;
   }
 
   return (
-    <div>
-      <h3>{title}</h3>
-      {events &&
-        events.map(event => (
-          <div key={event._id} className="card mb-3">
-            <p className="card-header">
-              <Link
-                to={`/profile/${event.username}`}
-                style={{ fontWeight: 700 }}
-                className="text-light"
-              >
-                {event.username}
-              </Link>{' '}
-               Event on {event.date}
-            </p>
-            <div className="card-body">
-              <Link to={`/event/${event._id}`}>
-                <p>{event.name}</p>
+    <Container className='d-flex flex-column' fluid>
+      <Row className='w-100 h-100'>
+        <Col>
+          {events &&
+            events.map(event => (
+              <Link to={`/event/${event._id}`} style={{ textDecoration: 'none' }}>
+                <Card.Body className='d-flex flex-row justify-content-start align-items-center' key={event._id}>
+                  <Card.Text><h3>{event.name}</h3></Card.Text>
+                  <Card.Text>{event.date}</Card.Text>
+                  <Card.Text>{event.time}</Card.Text>
+                  <Card.Text>{event.address}</Card.Text>
+                  <Card.Text>{event.city}</Card.Text>
+                </Card.Body>
               </Link>
-            </div>
-          </div>
-        ))}
-    </div>
+            ))}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
