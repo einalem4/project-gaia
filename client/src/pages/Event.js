736fca43b835442816e1b7ad7a3d24486251c31a
einalem4 <melanie@codehangar.io>
2021-06-03 13:12:08 -0600

    fix comments to display right after submission

--- client/src/pages/Event.js
+++ client/src/pages/Event.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { useQuery } from '@apollo/react-hooks';
@@ -6,137 +6,118 @@ import { Button, Form, Container, Row, Col, Image } from 'react-bootstrap';
 import { CalendarEvent, Clock, GeoAlt } from 'react-bootstrap-icons';
 import { ADD_COMMENT, ADD_ATTENDEE } from '../utils/mutations';
 import { QUERY_SINGLE_EVENT } from '../utils/queries';
-
 import CommentList from '../components/CommentList';
 import AttendanceList from '../components/AttendanceList';
 import Auth from '../utils/auth';
 
 function Event() {
-    const { id: eventId } = useParams();
+  const { id: eventId } = useParams();
+  const { loading, data } = useQuery(QUERY_SINGLE_EVENT, {
+    variables: { id: eventId }
+  });
 
-    const { loading, data } = useQuery(QUERY_SINGLE_EVENT, {
-        variables: { id: eventId }
-    });
+  let event = data?.singleEvent || [];
 
-    let event = data?.singleEvent || [];
+  const [commentText, setCommentText] = useState('');
+  const [addComment] = useMutation(ADD_COMMENT)
 
-    const [commentText, setCommentText] = useState('');
-
-    const [addComment] = useMutation(ADD_COMMENT, {
-        update(cache, { data: { addComment } }) {
-            try {
-                const { event } = cache.readQuery({
-                    query: QUERY_SINGLE_EVENT,
-                    variables: { id: eventId }
-                })
-                cache.writeQuery({
-                    query: QUERY_SINGLE_EVENT,
-                    variables: { id: eventId },
-                    data: { event: { ...event, comments: [...event.comments, addComment] } }
-                })
-            } catch(e){
-                console.error(e)
-            }
-        }
-    });
-
-    const [addAttendee] = useMutation(ADD_ATTENDEE, {
-        update(cache, { data: { addAttendee } }) {
-            try {
-                const { event } = cache.readQuery({
-                    query: QUERY_SINGLE_EVENT,
-                    variables: { id: eventId }
-                })
-                cache.writeQuery({
-                    query: QUERY_SINGLE_EVENT,
-                    variables: { id: eventId },
-                    data: { event: { ...event, attendees: [...event.attendees, addAttendee] } }
-                })
-            } catch(e){
-                console.error(e)
-            }
-        }
-    });
-
-    if (loading) {
-        return <div>Loading...</div>
+  const [addAttendee] = useMutation(ADD_ATTENDEE, {
+    update(cache, { data: { addAttendee } }) {
+      try {
+        const { event } = cache.readQuery({
+          query: QUERY_SINGLE_EVENT,
+          variables: { id: eventId }
+        })
+        cache.writeQuery({
+          query: QUERY_SINGLE_EVENT,
+          variables: { id: eventId },
+          data: { event: { ...event, attendees: [...event.attendees, addAttendee] } }
+        })
+      } catch (e) {
+        console.error(e)
+      }
     }
+  });
 
-    const handleChange = event => {
-        if (event.target.value.length > 0){
-            setCommentText(event.target.value)
-        }
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  const handleChange = event => {
+    if (event.target.value.length > 0) {
+      setCommentText(event.target.value)
     }
+  }
 
-    const handleFormSubmit = async event => {
-        event.preventDefault();
-        try {
-            await addComment({
-                variables: {eventId, commentText}
-            });
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+    try {
+      await addComment({
+        variables: { eventId, commentText }
+      });
 
-            setCommentText('');
-        } catch (e) {
-            console.error(e)
-        }
+      setCommentText('');
+    } catch (e) {
+      console.error(e)
     }
+  }
 
-    const handleRSVP = async event => {
-        event.preventDefault();
+  const handleRSVP = async event => {
+    event.preventDefault();
 
-        try {
-            await addAttendee({
-                variables: { eventId }
-            })
-        } catch (e) {
-            console.error(e)
-        }
-    };
+    try {
+      await addAttendee({
+        variables: { eventId }
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  };
 
-    return (
-        <Container id='event' className='my-5'>
-            <Row className='event-details'>
-                <Col xs={12} md={6}>
-                    <Image src={event.image} className='event-img' fluid />
-                </Col>
-                <Col xs={12} md={6}>
-                    <h2 className='mt-3'>{event.name}</h2>
-                    <Container className='event-info p-0'>
-                        <div>
-                            <CalendarEvent className='event-icons' style={{marginLeft: '0px'}} /> {event.date} 
-                        </div>
-                        <div>
-                            <Clock className='event-icons' /> {event.time} 
-                        </div>
-                        <div>
-                            <GeoAlt className='event-icons geo-icon' /> {event.address}, {event.city}, {event.state}
-                        </div>
-                    </Container>
-                    <p>{event.description}</p>
-                    <Button variant="success" onClick={handleRSVP}>RSVP</Button>
-                    <h5 className='mt-4'>People Attending: {event.attendanceCount}</h5>
-                    {event.attendanceCount > 0 && <AttendanceList attendees={event.attendees} />}
-                </Col>
-            </Row>
+  return (
+    <Container id='event' className='my-5'>
+      <Row className='event-details'>
+        <Col xs={12} md={6}>
+          <Image src={event.image} className='event-img' fluid />
+        </Col>
+        <Col xs={12} md={6}>
+          <h2 className='mt-3'>{event.name}</h2>
+          <Container className='event-info p-0'>
+            <div>
+              <CalendarEvent className='event-icons' style={{ marginLeft: '0px' }} /> {event.date}
+            </div>
+            <div>
+              <Clock className='event-icons' /> {event.time}
+            </div>
+            <div>
+              <GeoAlt className='event-icons geo-icon' /> {event.address}, {event.city}, {event.state}
+            </div>
+          </Container>
+          <p>{event.description}</p>
+          <Button variant="success" onClick={handleRSVP}>RSVP</Button>
+          <h5 className='mt-4'>People Attending: {event.attendanceCount}</h5>
+          {event.attendanceCount > 0 && <AttendanceList attendees={event.attendees} />}
+        </Col>
+      </Row>
 
-            <Row className='event-comments'>
-                <Col>
-                    {Auth.loggedIn() && 
-                    <Form onSubmit={handleFormSubmit} className='mt-3'>
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label></Form.Label>
-                            <Form.Control onChange={handleChange} value={commentText} as="textarea" rows={3} placeholder='Leave Comment Here'/>
-                            <br/>
-                            <Button variant='primary' type='submit'>Post Comment</Button>
-                        </Form.Group>
-                    </Form>
-                    }
-                    <h3 className='mt-5'>Comments</h3>
-                    <CommentList comments={event.comments} commentCount={event.commentCount} />
-                </Col>
-            </Row>
-        </Container>
-    )
+      <Row className='event-comments'>
+        <Col>
+          {Auth.loggedIn() &&
+            <Form onSubmit={handleFormSubmit} className='mt-3'>
+              <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label></Form.Label>
+                <Form.Control onChange={handleChange} value={commentText} as="textarea" rows={3} placeholder='Leave Comment Here' />
+                <br />
+                <Button variant='primary' type='submit'>Post Comment</Button>
+              </Form.Group>
+            </Form>
+          }
+          <h3 className='mt-5'>Comments</h3>
+          <CommentList comments={event.comments} commentCount={event.commentCount} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Event;
